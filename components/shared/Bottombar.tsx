5a7b32ab5e20a396cff0d35ce7c02dc15b42c4f0
papunidze <papunidze07@gmail.com>
2023-12-14 14:58:26 +0400

    feat:topbar-style

---
+++ components/shared/Bottombar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const BottomBar = () => {
+  return (
+    <div className="fixed bottom-0 z-10 w-full rounded-t-3xl bg-glassmorphism p-4 backdrop-blur-lg xs:px-7 md:hidden">
+      Bottombar
+    </div>
+  );
+};
+
+export default BottomBar;
