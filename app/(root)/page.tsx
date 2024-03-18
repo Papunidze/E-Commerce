c02ef769c86a7fe0d01a5f9ff3ab44239a789b84
papunidze <papunidze07@gmail.com>
2024-03-18 00:55:07 -0700

    feat:home

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,32 @@
-import Card from "@/components/shared/Card";
+import Image from "next/image";
+import HomeImg from "@/public/assets/home.svg";
 
 export default function Home() {
   return (
-    <div className="z-10 p-4">
-      <Card />
+    <div className="flex w-full items-center p-2 flex-col md:flex-row gap-3 h-[calc(100vh_-_10rem)]">
+      <div className="flex items-start flex-col justify-center gap-2 w-full mt-5">
+        <h1 className="font-dancing font-semibold text-primary text-5xl">
+          Trendy Collection
+        </h1>
+        <h1 className="font-sans font-bold text-5xl">
+          Let`s Create 🔥 Your Own Style
+        </h1>
+        <h4 className="font-sans text-gray-400 font-semibold">
+          Style is something of us already has, all we need to do is find it.
+          So, let`s find it out
+        </h4>
+        <button className="button primary rounded-full">Start Shopping</button>
+      </div>
+      <div className="flex-[1 100%] hidden md:block">
+        <Image
+          src={HomeImg}
+          alt="home"
+          width={500}
+          height={500}
+          loading="lazy"
+          className="object-cover w-full rounded-lg"
+        />
+      </div>
     </div>
   );
 }
